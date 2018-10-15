3b103814755d6648bf4adc92cfc522c44f989985
Muhammad Tabraiz Hassan <tabraiz2017@namal.edu.pk>
2018-10-15 10:53:05 +0500

    Started working

---
+++ tollbooth.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class TollBooth{
+public:
+TollBooth()
+{
+number_of_cars=0;
+money_collected=0;
+
+}
+void payingCar()
+{
+
+}
+
+void nopayCar()
+{
+
+}
+int display()
+{
+
+}
+
+
+
+
+
+
+
+
+
+private:
+unsigned int number_of_cars;
+double money_collected;
+};
+
+int main(int argc, char const *argv[])
+{
+
+  return 0;
+}
